5434f8f8d02c63492f9c4b3bf8943b98e5a044f6
Christopher Robles <crobles@nerdwallet.com>
2022-10-17 01:09:41 -0700

    feat: solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(...nums) {
+  if (nums.length !== 2) { return 'ERROR' };
+  if (nums.filter(x => typeof x !== 'number').length) { return 'ERROR' };
+  if (nums.filter(x => x<0).length) { return 'ERROR' };
+  nums.sort();
+  return nums[1]*(nums[1]+1)/2 - nums[0]*(nums[0]-1)/2;
 };
 
 // Do not edit below this line

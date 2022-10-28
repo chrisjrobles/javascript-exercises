180d7540367180c9c43b9ab623a4b2c88bf09aa3
Christopher Robles <crobles@nerdwallet.com>
2022-10-27 21:58:43 -0700

    feat: solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, nextPerson) => {
+    const now = new Date().getFullYear();
+    const oldestAge = (oldest.yearOfDeath ?? now) - oldest.yearOfBirth;
+    const nextPersonAge = (nextPerson.yearOfDeath ?? now) - nextPerson.yearOfBirth;
+    return oldestAge > nextPersonAge ? oldest : nextPerson;
+  }, people[0]);
 };
 
 // Do not edit below this line

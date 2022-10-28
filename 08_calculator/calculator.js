662871b4f639314c9bdf5be61f649e4fe0baaba7
Christopher Robles <crobles@nerdwallet.com>
2022-10-27 21:58:18 -0700

    feat: solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, curr) => total+curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, curr) => total*curr, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x===0 || x===1) {
+    return 1;
+  } else {
+    return x * factorial(x-1);
+  }
 };
 
 // Do not edit below this line

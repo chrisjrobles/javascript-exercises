ae52a8fbc5a469f2872ca01173c0e0f19bcc97c5
Christopher Robles <crobles@nerdwallet.com>
2022-10-27 21:58:24 -0700

    feat: solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const cleanedWord = word.toLowerCase().replace(/[^a-z]/gi, '');
+  const reversedWord = cleanedWord.split('').reverse().join('');
+  return cleanedWord === reversedWord;
 };
 
 // Do not edit below this line
